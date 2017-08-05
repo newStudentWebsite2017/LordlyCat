2f1fb0ed07f06fc778a62aff7be50d889a945953
BlackDriver <958272677@qq.com>
2017-08-05 15:48:53 +0800

    防崩及性能优化

--- welcome2017/javascript/index.js
+++ welcome2017/javascript/index.js
@@ -3,14 +3,18 @@ var mainbody = document.querySelector('body');
 console.log(mainbody);
     function bemid(){
         var screenwidth = document.body.clientWidth;
-        console.log('resize')
-    // var screenheight = 
         var leftbox = document.querySelector('.leftbox');
         var rightbox = document.querySelector('.rightbox');
         var distance = (screenwidth - 1250 - 209*2)/2;
-        leftbox.style.left = distance + 'px';
-        rightbox.style.right = distance + 'px';
-        console.log(screenwidth);
+        if (distance <= 0) {
+            leftbox.style.left = 0 + 'px';
+            rightbox.style.right = 0 + 'px';
+        }else{
+            leftbox.style.left = distance + 'px';
+            rightbox.style.right = distance + 'px';
+        }
+        
+        
     }
     window.addEventListener("resize",bemid);
     
